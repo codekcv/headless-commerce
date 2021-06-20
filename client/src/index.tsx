9b61d12802749c36e5bbb3f3a1cc3f8ffdeec736
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-21 02:13:28 +0800

    client(feat) add redux provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,18 @@
-import ReactDOM from 'react-dom';
 import './index.css';
+
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from 'store';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
