b484cae30765c7adce68e9be660e20d065b3abbc
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-20 22:01:23 +0800

    client(ref) ref dashboard static

--- client/src/menus/Dashboard/Dashboard.comp.tsx
+++ client/src/menus/Dashboard/Dashboard.comp.tsx
@@ -1,12 +1,14 @@
 import { Layout, Card, Col, Row } from 'antd';
+import styles from './Dashboard.module.css';
 
+const GRID_GAP = 16;
 const { Content } = Layout;
 
 const Dashboard = (): JSX.Element => {
   return (
-    <Content>
-      <Row>
-        <Col>
+    <Content className={styles.container}>
+      <Row className={styles.row} gutter={GRID_GAP}>
+        <Col span={12}>
           <Card
             title="The Pique Lab CRM Proof of Concept"
             bodyStyle={{
@@ -14,22 +16,43 @@ const Dashboard = (): JSX.Element => {
               justifyContent: 'space-between',
             }}
           >
-            <p style={{ width: '50%' }}>
+            <p>
               Hello, this is just a demo of a CRM Admin Dashboard created by
               Christian Villamin.
             </p>
 
-            <div style={{ width: '50%' }}>
-              <p>
-                Technology Stack: React, Redux, Recharts, React Hook Form, CSS
-                Modules, GraphQL, Apollo Client,
-              </p>
+            <p>
+              Technology Stack: React, Redux, Recharts, React Hook Form, CSS
+              Modules, GraphQL, Apollo Client.
+              <br />
+              <br />
+              GitHub:&nbsp;
+              <a href="https://github.com/codekcv/admin-panel">admin-panel</a>
+            </p>
+          </Card>
+        </Col>
 
-              <p>
-                GitHub:{' '}
-                <a href="https://github.com/codekcv/admin-panel">admin-panel</a>
-              </p>
-            </div>
+        <Col span={12}>
+          <Card
+            title="The Pique Lab CRM Proof of Concept"
+            bodyStyle={{
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+          >
+            <p>
+              Hello, this is just a demo of a CRM Admin Dashboard created by
+              Christian Villamin.
+            </p>
+
+            <p>
+              Technology Stack: React, Redux, Recharts, React Hook Form, CSS
+              Modules, GraphQL, Apollo Client.
+              <br />
+              <br />
+              GitHub:&nbsp;
+              <a href="https://github.com/codekcv/admin-panel">admin-panel</a>
+            </p>
           </Card>
         </Col>
       </Row>
