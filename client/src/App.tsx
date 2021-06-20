e7b5b79c841e7ec85b72fd3f101bba2a45dc7394
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-20 20:57:08 +0800

    client(feat) app menu routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,39 @@
 import MainLayout from 'components/MainLayout/MainLayout.comp';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import menus from 'menus/menus';
 
-const App = (): JSX.Element => (
-  <MainLayout>
-    <div>Item</div>
-  </MainLayout>
-);
+const App = (): JSX.Element => {
+  return (
+    <Router>
+      <MainLayout>
+        <Switch>
+          {menus.map((route) => {
+            if (route?.component) {
+              const mainMenu = (
+                <Route key={route.path} path={route.path} exact>
+                  {route.component}
+                </Route>
+              );
+
+              return mainMenu;
+            }
+
+            const subMenu = route.subroutes.map((subroute) => {
+              const path = `${route.path}${subroute.path}`;
+
+              return (
+                <Route key={path} path={path} exact>
+                  {subroute.component}
+                </Route>
+              );
+            });
+
+            return subMenu;
+          })}
+        </Switch>
+      </MainLayout>
+    </Router>
+  );
+};
 
 export default App;
