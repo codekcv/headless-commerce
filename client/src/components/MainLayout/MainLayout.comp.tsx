e556db14575f73fb3ddb9b21b66e34adb3d54155
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-21 10:34:09 +0800

    client(ref) main layout adjust

--- client/src/components/MainLayout/MainLayout.comp.tsx
+++ client/src/components/MainLayout/MainLayout.comp.tsx
@@ -1,96 +1,30 @@
-import { Layout, Menu } from 'antd';
-import menus from 'menus/menus';
+import { Layout } from 'antd';
+import MainHeader from 'components/MainHeader';
+import MainSider from 'components/MainSider';
 import { ReactNode, useState } from 'react';
-import { Link } from 'react-router-dom';
 
-import logo from '../../logo.svg';
 import styles from './MainLayout.module.css';
 
-const SIDER_WIDTH = 200;
-const HEADER_HEIGHT = 64;
-const { Sider, Header } = Layout;
-const { Item, SubMenu } = Menu;
+export const SIDER_WIDTH = 200;
+export const HEADER_HEIGHT = 64;
 
 type Props = {
   children: ReactNode;
 };
 
-const menuItems = menus.map((menu) => {
-  if (menu?.subroutes) {
-    return (
-      <SubMenu key={menu.title} icon={menu.icon} title={menu.title}>
-        {menu.subroutes.map((subroute) => {
-          return (
-            <Item key={subroute.title} icon={subroute.icon}>
-              <Link to={`${menu.path}${subroute.path}`}>{subroute.title}</Link>
-            </Item>
-          );
-        })}
-      </SubMenu>
-    );
-  }
-
-  return (
-    <Item key={menu.title} icon={menu.icon}>
-      <Link to={menu.path}>{menu.title}</Link>
-    </Item>
-  );
-});
-
-const defaultOpenKeys = menus
-  .filter((menu) => !!menu?.subroutes)
-  .map((menu) => menu.title);
-
 const MainLayout = ({ children }: Props): JSX.Element => {
-  const [collapsed, setCollapsed] = useState(false);
+  const collapseState = useState(false);
+  const [collapsed] = collapseState;
 
   return (
     <Layout className={styles.container}>
-      <Sider
-        className={styles.sider}
-        width={SIDER_WIDTH}
-        collapsible
-        collapsed={collapsed}
-        onCollapse={(e: boolean) => setCollapsed(e)}
-      >
-        <div className={styles.logoContainer} style={{ height: HEADER_HEIGHT }}>
-          <img
-            className={styles.logo}
-            src={logo}
-            width={48}
-            alt="logo"
-            style={{
-              marginLeft: collapsed ? 16 : 8,
-            }}
-          />
-
-          <h1
-            className={styles.logoTitle}
-            style={{
-              opacity: collapsed ? 0 : 1,
-              left: collapsed ? 68 : 56,
-            }}
-          >
-            Admin Panel
-          </h1>
-        </div>
-
-        <Menu
-          mode="inline"
-          theme="dark"
-          defaultSelectedKeys={[menus[0].title]}
-          defaultOpenKeys={defaultOpenKeys}
-        >
-          {menuItems}
-        </Menu>
-      </Sider>
+      <MainSider collapseState={collapseState} width={SIDER_WIDTH} />
 
       <Layout
         className={styles.contentContainer}
         style={{ marginLeft: collapsed ? 80 : SIDER_WIDTH }}
       >
-        <Header
-          className={styles.header}
+        <MainHeader
           style={{
             width: `calc(100% - ${collapsed ? 80 : SIDER_WIDTH}px)`,
             height: HEADER_HEIGHT,
