0b77913f5075230bc52743a872169e6034e880f8
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-21 23:25:43 +0800

    c.feat(dashboard) mv util to const. add dummy data

---
+++ client/src/menus/Dashboard/Dashboard.const.tsx
@@ -0,0 +1,124 @@
+import { Space, Tag } from 'antd';
+
+export const chartDummyData = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+type DataType = {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+  tags: string[];
+};
+
+export const dataSource: DataType[] = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
+export const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text: string): JSX.Element => <a href="/">{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: (tags: string[]): JSX.Element[] =>
+      tags.map((tag) => {
+        let color = tag.length > 5 ? 'geekblue' : 'green';
+        if (tag === 'loser') {
+          color = 'volcano';
+        }
+        return (
+          <Tag color={color} key={tag}>
+            {tag.toUpperCase()}
+          </Tag>
+        );
+      }),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_: string, record: DataType): JSX.Element => (
+      <Space size="middle">
+        <a href="/">Invite {record.name}</a>
+        <a href="/">Delete</a>
+      </Space>
+    ),
+  },
+];
