d19c78a0f4c268ba3a412b657fa138e5b62aceaf
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-21 10:33:20 +0800

    client(feat) separate sider component

---
+++ client/src/components/MainSider/MainSider.comp.tsx
@@ -0,0 +1,67 @@
+import { Layout, Menu } from 'antd';
+import { BasicProps } from 'antd/lib/layout/layout';
+import { HEADER_HEIGHT } from 'components/MainLayout/MainLayout.comp';
+import menus from 'menus/menus';
+import { Dispatch, SetStateAction } from 'react';
+
+import logo from '../../logo.svg';
+import { defaultOpenKeys, menuItems } from './MainSider.const';
+import styles from './MainSider.module.css';
+
+const { Sider } = Layout;
+
+type Props = BasicProps & {
+  collapseState: [boolean, Dispatch<SetStateAction<boolean>>];
+  width: number;
+};
+
+const MainSider = ({ collapseState, width }: Props): JSX.Element => {
+  const [collapsed, setCollapsed] = collapseState;
+
+  const onCollapse = (e: boolean) => {
+    setCollapsed(e);
+  };
+
+  return (
+    <Sider
+      className={styles.sider}
+      width={width}
+      collapsible
+      collapsed={collapsed}
+      onCollapse={onCollapse}
+    >
+      <div className={styles.logoContainer} style={{ height: HEADER_HEIGHT }}>
+        <img
+          className={styles.logo}
+          src={logo}
+          width={48}
+          alt="logo"
+          style={{
+            marginLeft: collapsed ? 16 : 8,
+          }}
+        />
+
+        <h1
+          className={styles.logoTitle}
+          style={{
+            opacity: collapsed ? 0 : 1,
+            left: collapsed ? 68 : 56,
+          }}
+        >
+          Admin Panel
+        </h1>
+      </div>
+
+      <Menu
+        mode="inline"
+        theme="dark"
+        defaultSelectedKeys={[menus[0].title]}
+        defaultOpenKeys={defaultOpenKeys}
+      >
+        {menuItems}
+      </Menu>
+    </Sider>
+  );
+};
+
+export default MainSider;
---
+++ client/src/components/MainSider/MainSider.const.tsx
@@ -0,0 +1,35 @@
+import { Menu } from 'antd';
+import menus from 'menus/menus';
+import { Link } from 'react-router-dom';
+
+const { Item, SubMenu } = Menu;
+
+export const menuItems = menus.map((menu) => {
+  if (!menu?.subroutes) {
+    const mainItem = (
+      <Item key={menu.title} icon={menu.icon}>
+        <Link to={menu.path}>{menu.title}</Link>
+      </Item>
+    );
+
+    return mainItem;
+  }
+
+  const subItems = (
+    <SubMenu key={menu.title} icon={menu.icon} title={menu.title}>
+      {menu.subroutes.map((subroute) => {
+        return (
+          <Item key={subroute.title} icon={subroute.icon}>
+            <Link to={`${menu.path}${subroute.path}`}>{subroute.title}</Link>
+          </Item>
+        );
+      })}
+    </SubMenu>
+  );
+
+  return subItems;
+});
+
+export const defaultOpenKeys = menus
+  .filter((menu) => !!menu?.subroutes)
+  .map((menu) => menu.title);
