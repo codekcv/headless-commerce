df8432e0fe7be8cbb933bb39b4a36c25d54e9aff
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-27 01:44:57 +0800

    s.feat(schema) nexus make schema

---
+++ server/api/schema.ts
@@ -0,0 +1,15 @@
+import { makeSchema } from 'nexus';
+import { join } from 'path';
+import * as types from '../gql';
+
+export const schema = makeSchema({
+  types,
+  outputs: {
+    typegen: join(__dirname, '..', 'nexus-typegen.ts'),
+    schema: join(__dirname, '..', 'nexus-schema.graphql'),
+  },
+  contextType: {
+    module: join(__dirname, './context.ts'),
+    export: 'Context',
+  },
+});
