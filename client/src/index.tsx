7e34212d1d652bf34b9002eb2e3c21d0605ea2b6
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-27 05:33:20 +0800

    c.feat(apollo) connect apollo client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import './index.css';
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from 'store';
@@ -7,10 +8,22 @@ import store from 'store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const uri =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:4000/'
+    : process.env.URI;
+
+const client = new ApolloClient({
+  uri,
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
@@ -18,3 +31,27 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+/*
+const httpLink = new HttpLink({ uri });
+
+const cleanTypeName = new ApolloLink((operation, forward) => {
+  console.log(33, operation);
+  if (operation.variables) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const omitTypename = (key: string, value: any) => {
+      console.log(key, value);
+      return key === '__typename' ? undefined : value;
+    };
+
+    operation.variables = JSON.parse(
+      JSON.stringify(operation.variables),
+      omitTypename
+    );
+  }
+  return forward(operation).map((data) => data);
+});
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const httpLinkWithErrorHandling = ApolloLink.from([cleanTypeName, httpLink]);
+*/
