8a4d7d79df6b68a142db723195f2cd63fd32576b
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-27 16:12:34 +0800

    s.feat(util) obj undefined entry remove

---
+++ server/utils/getObjValid.ts
@@ -0,0 +1,16 @@
+/**
+ * Remove entries with undefined values in an object.
+ */
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const getObjTruth = (obj: Record<string, any>): Record<string, any> => {
+  const buildValidObj: typeof obj = {};
+
+  Object.entries(obj).forEach(([key, value]) => {
+    if (value === undefined) buildValidObj[key] = value;
+  });
+
+  return buildValidObj;
+};
+
+export default getObjTruth;
