d369c4f12db0943653f963c8563bb3b989a76fd8
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-27 16:04:28 +0800

    s.feat(admin) obj update

--- server/gql/Admin/Admin.object.ts
+++ server/gql/Admin/Admin.object.ts
@@ -1,9 +1,11 @@
 import { objectType } from 'nexus';
 
-export const AdminObject = objectType({
+export const ADMIN = objectType({
   name: 'Admin',
-  definition(t) {
+  definition: (t) => {
     t.nonNull.int('id');
+    t.nonNull.string('username');
+    t.nonNull.string('password');
     t.nonNull.string('firstName');
     t.nonNull.string('lastName');
     t.nonNull.string('email');
--- server/gql/Admin/Admin.query.ts
+++ server/gql/Admin/Admin.query.ts
@@ -1,6 +1,7 @@
 import { queryField } from 'nexus';
 
-export const AdminQuery = queryField('getAdmin', {
+export const ADMIN_GET = queryField('adminGet', {
   type: 'Admin',
+  authorize: (_, __, ctx) => ctx.auth.ok,
   resolve: (_root, _args, ctx) => ctx.db.admin,
 });
