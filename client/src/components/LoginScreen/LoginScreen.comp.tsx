4d9d3a7ab4de3a8243522e0c15848c4575006520
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-27 23:17:25 +0800

    c.feat(login) BE integration

--- client/src/components/LoginScreen/LoginScreen.comp.tsx
+++ client/src/components/LoginScreen/LoginScreen.comp.tsx
@@ -1,8 +1,9 @@
-import { gql, useLazyQuery, useMutation } from '@apollo/client';
+import { gql, useLazyQuery, useMutation, useQuery } from '@apollo/client';
 import { Button, Card, Form, Input, Layout, Typography } from 'antd';
-import { useEffect } from 'react';
+import { useState } from 'react';
 import { useAppDispatch } from 'store';
 import { adminActions } from 'store/adminSlice';
+import rmvTypename from 'utils/rmvTypename';
 
 import styles from './LoginScreen.module.css';
 
@@ -10,49 +11,45 @@ const { Title, Text } = Typography;
 const { Item } = Form;
 const { Password } = Input;
 
-const ADMIN_GET = gql`
+export const ADMIN_GET_LOGIN_INFO = gql`
   {
-    adminGet {
+    adminGetLoginInfo {
       username
       password
     }
   }
 `;
 
-const ADMIN_LOGIN = gql`
-  mutation {
-    adminLogin(username: "codekcv", password: "plaintext")
+export const ADMIN_LOGIN = gql`
+  mutation adminLogin($username: String!, $password: String!) {
+    adminLogin(username: $username, password: $password)
   }
 `;
 
 const LoginScreen = (): JSX.Element => {
   const dispatch = useAppDispatch();
+  const [isLoading, setIsLoading] = useState(false);
+  const { error, data } = useQuery(ADMIN_GET_LOGIN_INFO);
+  const [adminLogin] = useMutation(ADMIN_LOGIN);
 
-  const [adminGet, { loading, error, data, called }] = useLazyQuery(ADMIN_GET);
-  const [login, { data: result }] = useMutation(ADMIN_LOGIN);
+  const onFinish = async (e: any) => {
+    setIsLoading(true);
 
-  const onFinish = () => {
-    dispatch(adminActions.setIsLoggedIn(true));
+    try {
+      const { username, password } = e;
+
+      await adminLogin({ variables: { username, password } });
+
+      dispatch(adminActions.setIsLoggedIn(true));
+    } catch (err) {
+      // eslint-disable-next-line no-alert
+      alert(err);
+    }
+
+    setIsLoading(false);
   };
-
-  useEffect(() => {
-    login();
-  }, [login]);
-
-  useEffect(() => {
-    if (result) adminGet();
-  }, [adminGet, result]);
 
   if (error) return <p>{`Error! ${error.message}`}</p>;
-
-  const isLoading = !called || loading;
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const rmvTypename = (obj: any) => {
-    const newObj = { ...obj };
-    delete newObj.__typename;
-    return newObj;
-  };
 
   return (
     <Layout className={styles.layout}>
@@ -87,7 +84,12 @@ const LoginScreen = (): JSX.Element => {
           </Item>
 
           <Item className={styles.item}>
-            <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+            <Button
+              type="primary"
+              htmlType="submit"
+              style={{ width: '100%' }}
+              disabled={isLoading}
+            >
               Submit
             </Button>
           </Item>
@@ -96,7 +98,7 @@ const LoginScreen = (): JSX.Element => {
         <Text type="secondary">
           <pre style={{ display: 'grid', alignItems: 'center', height: 110 }}>
             {JSON.stringify(
-              isLoading ? {} : rmvTypename(data.adminGet),
+              !data ? { fetching: true } : rmvTypename(data.adminGetLoginInfo),
               undefined,
               2
             )}
