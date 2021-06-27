597becf7098e97847ada695bd3c509e8955699fb
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-27 16:02:12 +0800

    s.dev(lint) include import and prettier. fixes eslint clash

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -3,25 +3,31 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['airbnb-base'],
+  extends: [
+    'airbnb-base',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
   rules: {
     'import/prefer-default-export': 'off',
-
     'import/extensions': [
       'error',
       'ignorePackages',
       { ts: 'never', tsx: 'never' },
     ],
 
-    // Others
-    'linebreak-style': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
   ignorePatterns: ['.eslintrc.js', 'nexus-typegen.ts'],
   settings: {
