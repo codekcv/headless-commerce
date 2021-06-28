19c7419eca84860675745d6e2ffbcd5a0d19ad87
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-29 01:46:57 +0800

    s.feat(customer) add age type

--- server/gql/Customer/Customer.object.ts
+++ server/gql/Customer/Customer.object.ts
@@ -8,6 +8,7 @@ export const CUSTOMER_OBJECT = objectType({
     t.nonNull.string('password');
     t.nonNull.string('firstName');
     t.nonNull.string('lastName');
+    t.nonNull.int('age');
     t.nonNull.string('email');
     t.nonNull.list.field('itemsBought', { type: 'Item' });
   },
