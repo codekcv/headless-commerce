2550e428994701d6d75a116324dc2038d45d298c
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-29 01:47:27 +0800

    s.dev(db) add faker db

--- server/api/db.ts
+++ server/api/db.ts
@@ -1,3 +1,4 @@
+import faker from 'faker';
 import { nanoid } from 'nanoid';
 
 import { NexusGenObjects } from '../nexus-typegen';
@@ -6,6 +7,37 @@ export interface Db {
   admin: NexusGenObjects['Admin'];
   customers: Array<NexusGenObjects['Customer']>;
   items: Array<NexusGenObjects['Item']>;
+}
+
+const customers = [];
+const items = [];
+
+faker.seed(1);
+
+for (let i = 0; i < 10; i += 1) {
+  const customer: NexusGenObjects['Customer'] = {
+    id: nanoid(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    age: Math.floor(Math.random() * (60 - 10) + 10),
+    email: faker.internet.email(),
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    itemsBought: [],
+  };
+
+  customers.push(customer);
+}
+
+for (let i = 0; i < 10; i += 1) {
+  const item: NexusGenObjects['Item'] = {
+    id: nanoid(),
+    name: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    price: parseFloat(faker.commerce.price()),
+  };
+
+  items.push(item);
 }
 
 // Temporary in-memory database.
@@ -18,14 +50,6 @@ export const db: Db = {
     lastName: 'Ipsum',
     email: 'loremipsum@email.com',
   },
-  customers: [],
-  items: [
-    {
-      id: nanoid(),
-      name: 'Apple',
-      description:
-        'A tasty snack. But did you know that shinigami only eat apples?',
-      price: 420.69,
-    },
-  ],
+  customers,
+  items,
 };
