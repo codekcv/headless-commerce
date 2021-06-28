b88e6cdfcf0c7748dc79d33c22ac104421913c83
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-29 01:51:51 +0800

    c.feat(header) query admin info

--- client/src/components/MainHeader/MainHeader.comp.tsx
+++ client/src/components/MainHeader/MainHeader.comp.tsx
@@ -1,5 +1,6 @@
 import { BellOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
-import { Avatar, Badge, Layout, Typography } from 'antd';
+import { gql, useQuery } from '@apollo/client';
+import { Avatar, Badge, Layout, Spin, Typography } from 'antd';
 import { BasicProps } from 'antd/lib/layout/layout';
 
 import styles from './MainHeader.module.css';
@@ -8,24 +9,40 @@ const ICON_SIZE = 18;
 const { Header } = Layout;
 const { Text } = Typography;
 
-const MainHeader = (props: BasicProps): JSX.Element => (
-  <Header className={styles.header} {...props}>
-    <div className={styles.leftSide}>Left</div>
+const ADMIN_GET = gql`
+  {
+    adminGet {
+      username
+      firstName
+    }
+  }
+`;
 
-    <div className={styles.rightSide}>
-      <Badge className={styles.item} count={5} offset={[4, -4]}>
-        <BellOutlined style={{ fontSize: ICON_SIZE }} />
-      </Badge>
+const MainHeader = (props: BasicProps): JSX.Element => {
+  const { data } = useQuery(ADMIN_GET);
 
-      <SettingOutlined
-        className={styles.item}
-        style={{ fontSize: ICON_SIZE }}
-      />
+  return (
+    <Header className={styles.header} {...props}>
+      <div className={styles.leftSide}>Left</div>
 
-      <Avatar className={styles.item} size={32} icon={<UserOutlined />} />
-      <Text className={styles.item}>Admin 01</Text>
-    </div>
-  </Header>
-);
+      <div className={styles.rightSide}>
+        <Badge className={styles.item} count={5} offset={[4, -4]}>
+          <BellOutlined style={{ fontSize: ICON_SIZE }} />
+        </Badge>
+
+        <SettingOutlined
+          className={styles.item}
+          style={{ fontSize: ICON_SIZE }}
+        />
+
+        <Avatar className={styles.item} size={32} icon={<UserOutlined />} />
+
+        <Text className={styles.item}>
+          {data?.adminGet?.username ?? <Spin />}
+        </Text>
+      </div>
+    </Header>
+  );
+};
 
 export default MainHeader;
