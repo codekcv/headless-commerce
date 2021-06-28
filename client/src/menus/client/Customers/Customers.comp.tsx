616eee059f0142be885e0c61ab242ca916e966f3
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-29 01:52:12 +0800

    s.feat(customer) fill table from query

--- client/src/menus/client/Customers/Customers.comp.tsx
+++ client/src/menus/client/Customers/Customers.comp.tsx
@@ -1,13 +1,23 @@
+import { useQuery } from '@apollo/client';
 import { Layout, Table } from 'antd';
 
-import { columns, dataSource } from './Customers.const';
+import { columns, CUSTOMER_GET_MANY } from './Customers.const';
 
 const { Content } = Layout;
 
-const Customers = (): JSX.Element => (
-  <Content>
-    <Table dataSource={dataSource} columns={columns} />
-  </Content>
-);
+const Customers = (): JSX.Element => {
+  const { data } = useQuery(CUSTOMER_GET_MANY);
+
+  return (
+    <Content>
+      <Table
+        dataSource={data?.customerGetMany ?? []}
+        columns={columns}
+        loading={!data}
+        rowKey="id"
+      />
+    </Content>
+  );
+};
 
 export default Customers;
--- client/src/menus/client/Customers/Customers.const.tsx
+++ client/src/menus/client/Customers/Customers.const.tsx
@@ -1,75 +1,61 @@
-import { Space, Tag } from 'antd';
+import { gql } from '@apollo/client';
+import { Space } from 'antd';
 
-type DataType = {
-  key: string;
-  name: string;
-  age: number;
-  address: string;
-  tags: string[];
-};
-
-export const dataSource: DataType[] = [
+export const CUSTOMER_GET_MANY = gql`
   {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-];
+    customerGetMany {
+      id
+      username
+      firstName
+      lastName
+      age
+      email
+      password
+    }
+  }
+`;
 
 export const columns = [
   {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: (text: string): JSX.Element => <a href="/">{text}</a>,
+    title: 'Customer ID',
+    dataIndex: 'id',
+    key: 'id',
+    render: (id: string): string => id,
+  },
+  {
+    title: 'Username',
+    dataIndex: 'username',
+    key: 'username',
+    render: (username: string): string => username,
+  },
+  {
+    title: 'First Name',
+    dataIndex: 'firstName',
+    key: 'firstName',
+    render: (firstName: string): string => firstName,
+  },
+  {
+    title: 'Last Name',
+    dataIndex: 'lastName',
+    key: 'lastName',
+    render: (lastName: string): string => lastName,
   },
   {
     title: 'Age',
     dataIndex: 'age',
     key: 'age',
+    render: (age: number): number => age,
   },
   {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (tags: string[]): JSX.Element[] =>
-      tags.map((tag) => {
-        let color = tag.length > 5 ? 'geekblue' : 'green';
-        if (tag === 'loser') {
-          color = 'volcano';
-        }
-        return (
-          <Tag color={color} key={tag}>
-            {tag.toUpperCase()}
-          </Tag>
-        );
-      }),
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+    render: (email: string): string => email,
   },
   {
     title: 'Action',
     key: 'action',
-    render: (_: string, record: DataType): JSX.Element => (
+    render: (_: string, record: any): JSX.Element => (
       <Space size="middle">
         <a href="/">Invite {record.name}</a>
         <a href="/">Delete</a>
