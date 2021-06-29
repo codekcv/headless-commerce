2944593a0a614e437a687e7d2aa746c4ea76b7b1
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-30 05:13:31 +0800

    c.feat(notification) empty after view

--- client/src/components/MainHeader/Notification/Notification.comp.tsx
+++ client/src/components/MainHeader/Notification/Notification.comp.tsx
@@ -1,5 +1,5 @@
 import { BellOutlined } from '@ant-design/icons';
-import { Badge, Popover } from 'antd';
+import { Badge, Empty, Popover } from 'antd';
 import { ReactElement, useState } from 'react';
 
 const ICON_SIZE = 18;
@@ -15,14 +15,25 @@ const content = (
 );
 
 const Notification = (): ReactElement => {
-  const [viewed, setViewed] = useState(false);
+  const [viewed, setViewed] = useState<boolean | null>(null);
+  const [done, setDone] = useState(false);
 
   return (
     <Popover
       placement="bottomRight"
       title="Notification"
-      content={content}
+      content={
+        done ? (
+          <Empty
+            image={Empty.PRESENTED_IMAGE_SIMPLE}
+            description="No New Notification"
+          />
+        ) : (
+          content
+        )
+      }
       trigger="click"
+      onVisibleChange={(e) => !e && setDone(true)}
     >
       <Badge count={viewed ? 0 : 5} offset={[4, -4]}>
         <BellOutlined
