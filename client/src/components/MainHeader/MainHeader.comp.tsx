e41b9568d98024f440172f1c4877a4346c357cc5
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-30 03:28:29 +0800

    c.feat(header) buttons update

--- client/src/components/MainHeader/MainHeader.comp.tsx
+++ client/src/components/MainHeader/MainHeader.comp.tsx
@@ -1,48 +1,26 @@
-import { BellOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
-import { gql, useQuery } from '@apollo/client';
-import { Avatar, Badge, Input, Layout, Spin, Typography } from 'antd';
+import { Input, Layout, Space } from 'antd';
 import { BasicProps } from 'antd/lib/layout/layout';
 
+import Logout from './Logout';
 import styles from './MainHeader.module.css';
+import Notification from './Notification';
+import Settings from './Settings';
 
-const ICON_SIZE = 18;
 const { Header } = Layout;
-const { Text } = Typography;
-
-const ADMIN_GET = gql`
-  {
-    adminGet {
-      username
-      firstName
-    }
-  }
-`;
 
 const MainHeader = (props: BasicProps): JSX.Element => {
-  const { data } = useQuery(ADMIN_GET);
-
   return (
     <Header className={styles.header} {...props}>
-      <div className={styles.leftSide} style={{ width: '100%' }}>
-        <Input placeholder="Search..." />
-      </div>
+      <Input
+        placeholder="Search..."
+        style={{ width: '100%', marginRight: 24 }}
+      />
 
-      <div className={styles.rightSide}>
-        <Badge className={styles.item} count={5} offset={[4, -4]}>
-          <BellOutlined style={{ fontSize: ICON_SIZE }} />
-        </Badge>
-
-        <SettingOutlined
-          className={styles.item}
-          style={{ fontSize: ICON_SIZE }}
-        />
-
-        <Avatar className={styles.item} size={32} icon={<UserOutlined />} />
-
-        <Text className={styles.item}>
-          {data?.adminGet?.username ?? <Spin />}
-        </Text>
-      </div>
+      <Space className={styles.rightSide} size="large" align="center">
+        <Notification />
+        <Settings />
+        <Logout />
+      </Space>
     </Header>
   );
 };
