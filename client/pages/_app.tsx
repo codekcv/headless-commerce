59979e5987df028edf5595d0a3c87b9c7292f3b0
Christian Villamin <ChristianVillamin31@gmail.com>
2021-06-30 20:06:38 +0800

    s.feat(_app) root providers

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,38 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { Provider as ReduxProvider } from 'react-redux';
+import store from '../store';
+import MainLayout from 'components/MainLayout';
+import MenuPath from 'components/MenuPath';
+import formatPathCrumb from 'utils/formatPathCrumb';
+import { useRouter } from 'next/router';
+
+const uri =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:4000/'
+    : process.env.REACT_APP_GRAPHQL_ENDPOINT;
+
+const client = new ApolloClient({
+  uri,
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { pathname } = useRouter();
+
+  return (
+    <ApolloProvider client={client}>
+      <ReduxProvider store={store}>
+        <MainLayout>
+          <MenuPath
+            path={formatPathCrumb(pathname)}
+            component={<Component {...pageProps} />}
+          />
+        </MainLayout>
+      </ReduxProvider>
+    </ApolloProvider>
+  );
 }
-export default MyApp
+
+export default MyApp;
