fbd9451f91a91ebd295c1c72ef76ec0586ce5d79
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-01 22:30:24 +0800

    c.feat(testing) jest setup

---
+++ client/jest.setup.js
@@ -0,0 +1,31 @@
+import '@testing-library/jest-dom';
+
+// These are to fix an Ant Design error.
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+Object.defineProperty(window, 'matchMedia', {
+  value: () => {
+    return {
+      matches: false,
+      addListener: () => {},
+      removeListener: () => {},
+    };
+  },
+});
+
+jest.mock('./modules/Dashboard/ChartContainer/ChartContainer.comp', () => ({
+  __esModule: true,
+  default: () => null,
+}));
