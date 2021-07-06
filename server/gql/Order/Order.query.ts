2d992ec397a4ba70a8efe7ad01272b2054f36dad
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-06 10:31:18 +0800

    s.feat(object) query use prisma

--- server/gql/Order/Order.query.ts
+++ server/gql/Order/Order.query.ts
@@ -4,8 +4,8 @@ export const ORDER_GET_ONE = queryField('orderGetOne', {
   type: 'Order',
   args: { id: nonNull(idArg()) },
   authorize: (_, __, ctx) => ctx.auth.ok,
-  resolve: (_root, args, ctx) => {
-    const findOrder = ctx.db.orders.find((order) => order.id === args.id);
+  resolve: async (_root, args, ctx) => {
+    const findOrder = await ctx.db.order.findUnique({ where: { id: args.id } });
 
     if (!findOrder) {
       throw Error(`Custom with id ${args.id} does not exist.`);
@@ -21,16 +21,5 @@ export const ORDER_GET_MANY = queryField('orderGetMany', {
     filter: idArg(),
   },
   authorize: (_, __, ctx) => ctx.auth.ok,
-  resolve: (_root, _arg, ctx) => {
-    let { orders } = ctx.db;
-
-    orders = orders.map((order) => {
-      return {
-        ...order,
-        total: order.items.reduce((acc, cur) => acc + (cur?.price ?? 0), 0),
-      };
-    });
-
-    return orders;
-  },
+  resolve: async (_root, _arg, ctx) => ctx.db.order.findMany(),
 });
