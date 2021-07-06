c77609c82a0314feb474a299c0069411ee5240be
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-06 09:51:07 +0800

    s.feat(admin) ref and use prisma

--- server/gql/Admin/Admin.mutation.ts
+++ server/gql/Admin/Admin.mutation.ts
@@ -8,13 +8,18 @@ export const ADMIN_LOGIN = mutationField('adminLogin', {
     username: nonNull(stringArg()),
     password: nonNull(stringArg()),
   },
-  resolve: (_root, args, ctx) => {
-    const { admin } = ctx.db;
-    const isMatchUsername = admin.username === args.username;
-    const isMatchPassword = admin.password === args.password;
+  resolve: async (_root, args, ctx) => {
+    const admin = await ctx.db.admin.findUnique({
+      where: {
+        username: args.username,
+      },
+    });
 
-    if (isMatchUsername && isMatchPassword) {
+    const isMatchPassword = admin?.password === args.password;
+
+    if (admin && isMatchPassword) {
       ctx.auth.ok = true;
+      ctx.auth.admin = admin;
     } else {
       throw new Error('Invalid username or password.');
     }
@@ -28,7 +33,7 @@ export const ADMIN_LOGOUT = mutationField('adminLogout', {
   authorize: (_, __, ctx) => ctx.auth.ok,
   resolve: (_, __, ctx) => {
     ctx.auth.ok = false;
-    return `Logged out ${ctx.db.admin.username}!`;
+    return `${ctx.auth.admin?.username} logged out!`;
   },
 });
 
@@ -39,12 +44,26 @@ export const ADMIN_UPDATE = mutationField('adminUpdate', {
     lastName: stringArg(),
   },
   authorize: (_, __, ctx) => ctx.auth.ok,
-  resolve: (_, args, ctx) => {
-    const { admin } = ctx.db;
+  resolve: async (_, args, ctx) => {
+    const admin = await ctx.db.admin.findUnique({
+      where: {
+        email: 'mail@mail.mail',
+      },
+    });
+
+    if (!admin) {
+      throw new Error('No admin found.');
+    }
+
     const truthyArgs = getObjTruth(args);
     const updatedAdmin = { ...admin, ...truthyArgs };
 
-    ctx.db.admin = updatedAdmin;
+    await ctx.db.admin.update({
+      where: {
+        email: 'mail@mail.mail',
+      },
+      data: updatedAdmin,
+    });
 
     return updatedAdmin;
   },
