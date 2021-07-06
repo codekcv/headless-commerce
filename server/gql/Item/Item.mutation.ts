427be34b4d377744f4af9273ed2fa845ca022a60
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-06 19:02:58 +0800

    s.feat(item) tmprly disable mutation

--- server/gql/Item/Item.mutation.ts
+++ server/gql/Item/Item.mutation.ts
@@ -1,12 +1,14 @@
-import { floatArg, mutationField, nonNull, stringArg } from 'nexus';
+// import { floatArg, mutationField, nonNull, stringArg } from 'nexus';
 
-export const ITEM_CREATE_ONE = mutationField('itemCreateOne', {
-  type: 'Item',
-  args: {
-    name: nonNull(stringArg()),
-    description: nonNull(stringArg()),
-    price: nonNull(floatArg()),
-  },
-  authorize: (_, __, ctx) => ctx.auth.ok,
-  resolve: async (_root, args, ctx) => ctx.db.item.create({ data: args }),
-});
+// export const ITEM_CREATE_ONE = mutationField('itemCreateOne', {
+//   type: 'Item',
+//   args: {
+//     name: nonNull(stringArg()),
+//     description: nonNull(stringArg()),
+//     price: nonNull(floatArg()),
+//   },
+//   authorize: (_, __, ctx) => ctx.auth.ok,
+//   resolve: async (_root, args, ctx) => ctx.prisma.item.create({ data: args }),
+// });
+
+export {};
