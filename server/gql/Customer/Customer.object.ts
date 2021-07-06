4ef61c335895ac1f6fe32ca04abf2cdf6bfc2c50
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-06 10:23:07 +0800

    s.feat(customer) use nexus-prisma

--- server/gql/Customer/Customer.object.ts
+++ server/gql/Customer/Customer.object.ts
@@ -1,15 +1,16 @@
 import { objectType } from 'nexus';
+import { Customer } from 'nexus-prisma';
 
 export const CUSTOMER_OBJECT = objectType({
-  name: 'Customer',
+  name: Customer.$name,
   definition: (t) => {
-    t.nonNull.id('id');
-    t.nonNull.string('username');
-    t.nonNull.string('password');
-    t.nonNull.string('firstName');
-    t.nonNull.string('lastName');
-    t.nonNull.int('age');
-    t.nonNull.string('email');
-    t.nonNull.list.field('itemsBought', { type: 'Item' });
+    t.nonNull.field(Customer.id);
+    t.nonNull.field(Customer.username);
+    t.nonNull.field(Customer.password);
+    t.nonNull.field(Customer.firstName);
+    t.nonNull.field(Customer.lastName);
+    t.nonNull.field(Customer.age);
+    t.nonNull.field(Customer.email);
+    t.nonNull.field(Customer.itemsBought);
   },
 });
