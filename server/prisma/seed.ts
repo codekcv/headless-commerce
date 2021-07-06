55262808f119417765c6663ed892acd8351de85f
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-06 17:54:37 +0800

    s.ref(prisma) db seed script

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -6,73 +6,10 @@ import { PrismaClient } from '@prisma/client';
 import faker from 'faker';
 import { nanoid } from 'nanoid';
 
-const customers: any[] = [];
-const items: any[] = [];
-const orders: any[] = [];
-
-for (let i = 0; i < 10; i += 1) {
-  const customer = {
-    id: '-',
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    age: Math.floor(Math.random() * (60 - 10) + 10),
-    email: faker.internet.email(),
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
-    itemsBought: [],
-  };
-
-  customers.push(customer);
-}
-
-for (let i = 0; i < 10; i += 1) {
-  const item = {
-    id: '-',
-    name: faker.commerce.productName(),
-    description: faker.commerce.productDescription(),
-    price: parseFloat(faker.commerce.price()),
-  };
-
-  items.push(item);
-}
-
-const getRandomItems = () => {
-  const randomItems = [];
-  const randomTotal = Math.floor(Math.random() * (items.length - 1) + 1);
-
-  for (let i = 0; i < randomTotal; i += 1) {
-    const randomItemIndex = Math.floor(Math.random() * items.length);
-
-    randomItems.push(items[randomItemIndex]);
-  }
-
-  return randomItems;
-};
-
-for (let i = 0; i < 10; i += 1) {
-  const randomItems = getRandomItems();
-
-  const order = {
-    id: '-',
-    reference: nanoid(8).toUpperCase(),
-    customer: customers[Math.floor(Math.random() * customers.length)],
-    address: `${faker.address.zipCode()} ${faker.address.cityName()} ${faker.address.streetName()}`,
-    items: randomItems,
-    total: randomItems.reduce((acc, cur) => acc + (cur?.price ?? 0), 0),
-    orderDate: faker.datatype.datetime(),
-    status: 'ORDERED' as 'ORDERED' | 'DELIVERED' | 'CANCELLED',
-  };
-
-  orders.push(order);
-}
-
 const prisma = new PrismaClient();
 
 const main = async (): Promise<void> => {
   try {
-    console.log(0, customers);
-    console.log(1, items);
-    console.log(2, orders);
     // Admin
     await prisma.admin.upsert({
       where: {
@@ -88,47 +25,94 @@ const main = async (): Promise<void> => {
       update: {},
     });
 
+    // Item Views
+    // const itemViews: ItemView[] = [];
+
+    // for (let i = 0; i < 10; i += 1) {
+    //   const id = nanoid();
+
+    //   const item = await prisma.itemView.upsert({
+    //     where: { id },
+    //     create: {
+    //       id,
+    //       name: faker.commerce.productName(),
+    //       description: faker.commerce.productDescription(),
+    //       price: parseFloat(faker.commerce.price()),
+    //     },
+    //     update: {},
+    //   });
+
+    //   itemViews.push(item);
+    // }
+
     // Customers
-    for (const customer of customers) {
-      await prisma.customer.upsert({
-        where: { email: customer.email },
+    const customers = [];
+
+    for (let i = 0; i < 1; i += 1) {
+      const id = nanoid();
+
+      const customer = await prisma.customer.upsert({
+        where: { id },
         create: {
-          username: customer.username,
-          password: customer.password,
-          lastName: customer.lastName,
-          firstName: customer.firstName,
-          email: customer.email,
-          age: customer.age,
+          id,
+          username: faker.internet.userName(),
+          password: faker.internet.password(),
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          email: faker.internet.email(),
+          age: Math.floor(Math.random() * (60 - 10) + 10),
         },
         update: {},
       });
+
+      customers.push(customer);
     }
 
     // Items
-    for (const item of items) {
-      await prisma.item.upsert({
-        where: { id: item.id },
-        create: {
-          name: item.name,
-          description: item.description,
-          price: item.price,
-        },
-        update: {},
-      });
-    }
+
+    // const getId = (): string => {
+    //   return itemViews[Math.floor(Math.random() * items.length)].id;
+    // };
 
     // Orders
-    for (const order of orders) {
+    for (let i = 0; i < 2; i += 1) {
+      const id = nanoid();
+
       await prisma.order.upsert({
-        where: { id: order.id },
+        where: { id },
         create: {
-          reference: order.reference,
-          address: order.address,
-          status: order.status,
-          total: order.total,
-          customer: order.customer,
-          items: order.items,
-          orderDate: order.orderDate,
+          id: nanoid(),
+          reference: nanoid(8).toUpperCase(),
+          address: `${faker.address.zipCode()} ${faker.address.cityName()} ${faker.address.streetName()}`,
+          status: 'ORDERED',
+          total: 42,
+          orderDate: new Date(faker.datatype.datetime().toISOString()),
+          itemList: {
+            createMany: {
+              data: (() => {
+                const items = [];
+
+                for (let j = 0; j < 4; j += 1) {
+                  const item = {
+                    name: faker.commerce.productName(),
+                    description: faker.commerce.productDescription(),
+                    price: parseFloat(faker.commerce.price()),
+                  };
+
+                  items.push(item);
+                }
+
+                console.log('I:', items);
+
+                return items;
+              })(),
+            },
+          },
+          customer: {
+            connect: {
+              id: customers[0].id,
+            },
+          },
         },
         update: {},
       });
@@ -139,9 +123,5 @@ const main = async (): Promise<void> => {
     prisma.$disconnect();
   }
 };
-
-console.log(3, customers);
-console.log(4, items);
-console.log(5, orders);
 
 main();
