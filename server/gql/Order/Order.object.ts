ef30797687511735ad2da52a489dd351ede0fe03
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-06 10:28:15 +0800

    s.feat(order) use nexus-prisma and add order status enum

--- server/gql/Order/Order.object.ts
+++ server/gql/Order/Order.object.ts
@@ -1,21 +1,22 @@
 import { enumType, objectType } from 'nexus';
+import { Order } from 'nexus-prisma';
 
 export const ORDER = objectType({
-  name: 'Order',
+  name: Order.$name,
   definition: (t) => {
-    t.nonNull.id('id');
-    t.nonNull.string('reference');
-    t.nonNull.field('customer', { type: 'Customer' });
-    t.nonNull.string('address');
-    t.nonNull.list.field('items', { type: 'Item' });
-    t.nonNull.float('total');
-    t.nonNull.string('orderDate');
-    t.nonNull.field('status', { type: 'OrderStatus' });
+    t.nonNull.field(Order.id);
+    t.nonNull.field(Order.reference);
+    t.nonNull.field(Order.customer);
+    t.nonNull.field(Order.address);
+    t.nonNull.field(Order.items);
+    t.nonNull.field(Order.total);
+    t.nonNull.field(Order.orderDate);
+    t.nonNull.field(Order.status);
   },
 });
 
 export const ORDER_STATUS = enumType({
   name: 'OrderStatus',
-  members: ['ORDERED', 'DELIVERED', 'CANCALLED'],
+  members: ['ORDERED', 'DELIVERED', 'CANCELLED'],
   description: 'The status of the order.',
 });
