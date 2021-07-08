867c13279ed3689698b5bfd224d23fda854d58ba
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-09 07:55:43 +0800

    s.feat(server) use apollo koa

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,6 +1,31 @@
-import { server } from './server';
+import 'dotenv/config';
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  // eslint-disable-next-line no-console
-  console.log(`🚀 Server ready at ${url}`);
-});
+import { ApolloServer } from 'apollo-server-koa';
+import Koa from 'koa';
+
+import { context } from './context';
+import { schema } from './schema';
+
+const PORT = process.env.PORT || 4000;
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    schema,
+    context,
+    introspection: true,
+    playground: true,
+  });
+
+  await server.start();
+
+  const app = new Koa();
+  server.applyMiddleware({ app });
+
+  await new Promise((resolve) =>
+    app.listen({ port: PORT }, resolve as () => void)
+  );
+
+  console.log(`🚀 Server ready at port:${PORT}/${server.graphqlPath}`);
+}
+
+startApolloServer();
