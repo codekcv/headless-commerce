bc0f5e8eeb59c41f5153e423dbbc4999991621e1
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-10 16:19:13 +0800

    c.fix(_app) move checks to access provider | use memory token

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -7,20 +7,19 @@ import {
   InMemoryCache,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import AccessProvider from 'components/AccessProvider/AccessProvider';
 import MainLayout from 'components/MainLayout';
 import MenuPath from 'components/MenuPath';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
-import { adminActions } from 'store/adminSlice';
-import { getRefreshTokenCookie } from 'utils/refreshTokenCookie';
+import { getMemoryToken } from 'utils/refreshTokenCookie';
 
-import store, { useAppDispatch, useAppSelector } from '../store';
+import store from '../store';
 
 export const uri =
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:4000/'
+    ? 'http://localhost:4000'
     : process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT;
 
 const httpLink = createHttpLink({
@@ -29,12 +28,12 @@ const httpLink = createHttpLink({
 });
 
 const authLink = setContext((_, { headers }) => {
-  const accessToken = useAppSelector((state) => state.admin.accessToken);
+  const accessToken = getMemoryToken();
 
   return {
     headers: {
       ...headers,
-      authorization: accessToken ? `Bearer ${accessToken}` : '',
+      authorization: `Bearer ${accessToken}`,
     },
   };
 });
@@ -45,47 +44,20 @@ const client = new ApolloClient({
 });
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-  const router = useRouter();
-  const dispatch = useAppDispatch();
-  const isAuthorized = useAppSelector((state) => state.admin.isAuthorized);
-
-  useEffect(() => {
-    const refreshToken = getRefreshTokenCookie();
-
-    if (refreshToken && !isAuthorized) {
-      const getAccessToken = async () => {
-        const res = await fetch(`${uri}/refresh_token`, {
-          method: 'POST',
-          headers: { Authorization: `Bearer ${refreshToken}` },
-        });
-
-        if (res) {
-          const newAccessToken = await res.json();
-
-          dispatch(adminActions.setAccessToken(newAccessToken));
-          dispatch(adminActions.setIsAuthorized(true));
-          dispatch(adminActions.setIsLoggedIn(true));
-        } else {
-          // Refresh Token is expired or invalidated.
-          document.cookie =
-            'refreshToken=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
-        }
-      };
-
-      getAccessToken();
-    }
-  }, [dispatch, isAuthorized]);
+  const { pathname } = useRouter();
 
   return (
     <ReduxProvider store={store}>
       <ApolloProvider client={client}>
-        {router.pathname === '/' ? (
-          <Component {...pageProps} />
-        ) : (
-          <MainLayout>
-            <MenuPath component={<Component {...pageProps} />} />
-          </MainLayout>
-        )}
+        <AccessProvider>
+          {pathname === '/' ? (
+            <Component {...pageProps} />
+          ) : (
+            <MainLayout>
+              <MenuPath component={<Component {...pageProps} />} />
+            </MainLayout>
+          )}
+        </AccessProvider>
       </ApolloProvider>
     </ReduxProvider>
   );
