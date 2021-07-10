b37ce03684650b7ced0a71381c8108a660cbd775
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-10 10:19:57 +0800

    s.feat(auth)  verify token and use graphql shield

--- server/api/schema.ts
+++ server/api/schema.ts
@@ -1,18 +1,43 @@
-import { fieldAuthorizePlugin, makeSchema } from 'nexus';
+import { applyMiddleware } from 'graphql-middleware';
+import { allow, rule, shield } from 'graphql-shield';
+import { makeSchema } from 'nexus';
 import { join } from 'path';
 
 import * as types from '../gql';
+import { verifyAccessToken } from '../utils/verifyToken';
 
-export const schema = makeSchema({
-  types,
-  outputs: {
-    typegen: join(__dirname, '..', 'nexus-typegen.ts'),
-    schema: join(__dirname, '..', 'nexus-schema.graphql'),
-  },
-  contextType: {
-    module: join(__dirname, './context.ts'),
-    export: 'Context',
-  },
-  plugins: [fieldAuthorizePlugin()],
-  prettierConfig: join(__dirname, '/../../.prettierrc.js'),
+const authenticate = rule({ cache: 'contextual' })(async (_, __, ctx) => {
+  const { authorization } = ctx.request.headers;
+
+  if (!authorization) return false;
+
+  const token = authorization.split(' ')[1];
+
+  return verifyAccessToken(token);
 });
+
+const permissions = shield({
+  Query: {
+    '*': authenticate,
+  },
+  Mutation: {
+    '*': authenticate,
+    adminLogin: allow,
+  },
+});
+
+export const schema = applyMiddleware(
+  makeSchema({
+    types,
+    outputs: {
+      typegen: join(__dirname, '..', 'nexus-typegen.ts'),
+      schema: join(__dirname, '..', 'nexus-schema.graphql'),
+    },
+    contextType: {
+      module: join(__dirname, './context.ts'),
+      export: 'Context',
+    },
+    prettierConfig: join(__dirname, '/../../.prettierrc.js'),
+  }),
+  permissions
+);
