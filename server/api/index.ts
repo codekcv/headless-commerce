ca00af28975b8bf8fdd06ec05f86fdee468de439
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-10 10:15:30 +0800

    s.feat(index) use koa and add refresh token route

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,31 +1,44 @@
 import 'dotenv/config';
 
+import cors from '@koa/cors';
+import Router from '@koa/router';
 import { ApolloServer } from 'apollo-server-koa';
 import Koa from 'koa';
 
 import { context } from './context';
+import refreshToken from './refreshToken';
 import { schema } from './schema';
 
 const PORT = process.env.PORT || 4000;
 
-async function startApolloServer() {
+const startApolloServer = async () => {
   const server = new ApolloServer({
     schema,
-    context,
     introspection: true,
     playground: true,
+    context: ({ ctx }) => {
+      return { ...ctx, ...context };
+    },
   });
 
   await server.start();
 
   const app = new Koa();
+  const router = new Router();
+
+  router.post('/refresh_token', refreshToken);
+
+  app.use(cors({ origin: '*', credentials: true }));
+  app.use(router.routes()).use(router.allowedMethods());
+
   server.applyMiddleware({ app });
 
   await new Promise((resolve) =>
     app.listen({ port: PORT }, resolve as () => void)
   );
 
+  // eslint-disable-next-line no-console
   console.log(`🚀 Server ready at port:${PORT}/${server.graphqlPath}`);
-}
+};
 
 startApolloServer();
