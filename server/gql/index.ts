2ae02f5831bdc4de10284a8d7390b7bc322986dd
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-10 16:35:55 +0800

    s.feat(token) refresh token query

--- server/gql/index.ts
+++ server/gql/index.ts
@@ -1,4 +1,7 @@
+import jwt from 'jsonwebtoken';
 import { queryField } from 'nexus';
+
+import generateAccessToken from '../utils/generateAccessToken';
 
 export * from './Admin';
 export * from './Customer';
@@ -10,4 +13,35 @@ export * from './Scalar';
 export const HELLO_WORLD = queryField('helloWorld', {
   type: 'String',
   resolve: () => 'Hello World',
+});
+
+export const GET_NEW_ACCESS_TOKEN = queryField('getNewAccessToken', {
+  type: 'String',
+  resolve: async (_, __, ctx) => {
+    const refreshToken = ctx.cookies.get('refreshToken');
+
+    if (!refreshToken) {
+      throw new Error('No token.');
+    }
+
+    const admin = await ctx.prisma.admin.findUnique({
+      where: { refreshToken },
+    });
+
+    if (!admin) {
+      throw new Error('Invalid.');
+    }
+
+    if (!process.env.REFRESH_TOKEN_SECRET) {
+      throw new Error('Internal Server Error: REFRESH_TOKEN_SECRET');
+    }
+
+    try {
+      jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+      return generateAccessToken({ sub: admin.id });
+    } catch (err) {
+      throw new Error('Token is invalid/expired.');
+    }
+  },
 });
