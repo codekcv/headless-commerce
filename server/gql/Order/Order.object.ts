7b9546899caf210894e85411adc23929012c14ae
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-15 12:13:51 +0800

    s.feat(order) object update

--- server/gql/Order/Order.object.ts
+++ server/gql/Order/Order.object.ts
@@ -1,22 +1,18 @@
 import { enumType, objectType } from 'nexus';
-import { Order } from 'nexus-prisma';
+import { Order, OrderStatus } from 'nexus-prisma';
 
 export const ORDER = objectType({
   name: Order.$name,
   definition: (t) => {
     t.field(Order.id);
-    t.field(Order.reference);
+    t.field(Order.referenceId);
     t.field(Order.customer);
     t.field(Order.address);
-    t.field(Order.items);
+    t.field(Order.itemsInOrder);
     t.field(Order.total);
     t.field(Order.orderDate);
     t.field(Order.status);
   },
 });
 
-export const ORDER_STATUS = enumType({
-  name: 'OrderStatus',
-  members: ['ORDERED', 'DELIVERED', 'CANCELLED'],
-  description: 'The status of the order.',
-});
+export const ORDER_STATUS = enumType(OrderStatus);
