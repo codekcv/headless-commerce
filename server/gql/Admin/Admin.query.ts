3a77edc6f82efc885cdd3bb90d53d9d09422cf37
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-21 10:41:33 +0800

    s.ref(admin) use admin object

--- server/gql/Admin/Admin.query.ts
+++ server/gql/Admin/Admin.query.ts
@@ -1,11 +1,13 @@
 import { list, queryField } from 'nexus';
 
+import { ADMIN } from './Admin.object';
+
 export const ADMIN_GET = queryField('adminGet', {
-  type: 'Admin',
+  type: ADMIN,
   resolve: async (_, __, ctx) => ctx.prisma.admin.findFirst(),
 });
 
 export const ADMIN_GET_MANY = queryField('adminGetMany', {
-  type: list('Admin'),
+  type: list(ADMIN),
   resolve: async (_, __, ctx) => ctx.prisma.admin.findMany(),
 });
