97dbcab93ff58643e5b097ab1e37b6e820fc9234
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-21 22:26:47 +0800

    s.feat(admin) me query and get by id

--- server/gql/Admin/Admin.query.ts
+++ server/gql/Admin/Admin.query.ts
@@ -1,10 +1,34 @@
-import { list, queryField } from 'nexus';
+import { idArg, list, nonNull, queryField } from 'nexus';
 
 import { ADMIN } from './Admin.object';
 
+export const ADMIN_ME = queryField('adminMe', {
+  type: ADMIN,
+  resolve: (_, __, ctx) => {
+    const [admin] = ctx.me;
+
+    if (!admin) {
+      throw new Error('No admin found.');
+    }
+
+    return admin;
+  },
+});
+
 export const ADMIN_GET = queryField('adminGet', {
   type: ADMIN,
-  resolve: async (_, __, ctx) => ctx.prisma.admin.findFirst(),
+  args: {
+    id: nonNull(idArg()),
+  },
+  resolve: async (_, args, ctx) => {
+    const admin = await ctx.prisma.admin.findUnique({
+      where: {
+        id: args.id,
+      },
+    });
+
+    return admin;
+  },
 });
 
 export const ADMIN_GET_MANY = queryField('adminGetMany', {
