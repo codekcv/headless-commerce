bf6e755ab8e2d2b281e28c9a2839e983f0a3d9d3
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-22 08:19:37 +0800

    s.ref(util) get token

--- server/utils/verifyToken.ts
+++ server/utils/verifyToken.ts
@@ -1,29 +1,29 @@
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
-export const verifyAccessToken = (token: string): boolean => {
+export const getAccessToken = (
+  token: string
+): JwtPayload | string | undefined => {
   if (!process.env.ACCESS_TOKEN_SECRET) {
     throw new Error('Internal Server Error: ACCESS_TOKEN_SECRET');
   }
 
   try {
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
   } catch {
-    return false;
+    return undefined;
   }
-
-  return true;
 };
 
-export const verifyRefreshToken = (token: string): boolean => {
+export const getRefreshToken = (
+  token: string
+): JwtPayload | string | undefined => {
   if (!process.env.REFRESH_TOKEN_SECRET) {
     throw new Error('Internal Server Error: REFRESH_TOKEN_SECRET');
   }
 
   try {
-    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
+    return jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
   } catch {
-    return false;
+    return undefined;
   }
-
-  return true;
 };
